164d6f4b2650622244a8eb23134b037648a5f69d
lad475 <lad475@nyu.edu>
2018-02-14 14:44:24 -0500

    updated methods and comments in game script

--- tictactoe.js
+++ tictactoe.js
@@ -1,15 +1,19 @@
 var board = [];
 var bSize = 0;
 
+//If the board is 3x3, determine if there is a win
 function hasWon9(){
 
 }
+//If the board is 4x4, determine if there is a win
 function hasWon16(){
 
 }
+//If the board is 5x5, determine if there is a win
 function hasWon25(){
-	
+
 }
+//Determine if the game is over, and if so, determine who won.
 function hasWon(){
 	//Determine if anyone has won
 	if(bSize === 9){
@@ -23,15 +27,19 @@ function hasWon(){
 	}
 
 }
+//Draw the divs that represent the tic tac toe board
 function generateBoard(){
 	
 }
-function move(player){
+//The mechanism that makes the game play
+function play(){
 
 }
+//Reads the user's chosen board size and hides the initial div
 function start(){
 	bSize = document.querySelector("#inputText").value;
 }
+//First function called to start the game
 function main(){
 	start();
 }
