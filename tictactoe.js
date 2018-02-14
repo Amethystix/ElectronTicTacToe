dfdd748fde801cefcb58a01fdcfc042e1a2c1343
lad475 <lad475@nyu.edu>
2018-02-14 14:21:23 -0500

    First commit, setting things up

---
+++ tictactoe.js
@@ -0,0 +1,39 @@
+var board = [];
+var bSize = 0;
+
+function hasWon9(){
+
+}
+function hasWon16(){
+
+}
+function hasWon25(){
+	
+}
+function hasWon(){
+	//Determine if anyone has won
+	if(bSize === 9){
+
+	}
+	else if(bSize === 16){
+
+	}
+	else if(bSize === 25){
+
+	}
+
+}
+function generateBoard(){
+	
+}
+function move(player){
+
+}
+function start(){
+	bSize = document.querySelector("#inputText").value;
+}
+function main(){
+	start();
+}
+
+document.eventListener("DOMContentLoaded", main);
